1c70e3abdf9da84db1c15c317c14c5a4943b847d
caseyjramirez <caseyjramirez5@gmail.com>
2023-01-25 15:38:07 -0600

    Created the Fish class to inherit from the Animal Class

--- src/Fish.java
+++ src/Fish.java
@@ -1,2 +1,35 @@
-public class Fish {
+public class Fish extends Animal {
+    private int gills;
+    private int fins;
+
+    public Fish(String type, double weight, int gills, int fins) {
+        super(type, "small", weight);
+        this.gills = gills;
+        this.fins = fins;
+    }
+
+    private void moveMuscles() {
+        System.out.println("Move Muscles");
+    }
+
+    private void moveBackfin() {
+        System.out.println("Move back fin!");
+    }
+
+    @Override
+    public void move(String speed) {
+        System.out.println(type + " swims " + speed + " in the ocean.");
+        moveMuscles();
+        if (speed == "Fast") {
+            moveBackfin();
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Fish{" +
+                "gills=" + gills +
+                ", fins=" + fins +
+                "} " + super.toString();
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,13 @@ public class Main {
         Animal animal = new Animal("General Monkee", "Huge", 400);
         Dog dog = new Dog("Golden", 100, "Medium", "rounnd", "long");
         Dog defaultDoge = new Dog("Shelty", 20);
+        Fish goldFish = new Fish("Gold Fish", 4, 2, 4);
+
 
         doAnimalStuff(animal, "Slow");
         doAnimalStuff(dog, "V Fast");
         doAnimalStuff(defaultDoge, "Slow");
+        doAnimalStuff(goldFish, "Fast");
     }
 
     public static void doAnimalStuff(Animal animal, String speed) {
