7410f2dd41f7ddf13e32fea91d7df949bce1c143
caseyjramirez <caseyjramirez5@gmail.com>
2023-01-25 14:45:37 -0600

    Added Dog and Animal Class

---
+++ src/Dog.java
@@ -0,0 +1,6 @@
+public class Dog extends Animal {
+
+    public Dog(String type, String size, double weight) {
+        super(type, size, weight);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Animal animal = new Animal("General Monkee", "Huge", 400);
+        Dog dog = new Dog("Golden", "Medium", 100);
+
+        doAnimalStuff(animal, "Slow");
+        doAnimalStuff(dog, "V Fast");
+    }
+
+    public static void doAnimalStuff(Animal animal, String speed) {
+        animal.makeNoise();
+        animal.move(speed);
+        System.out.println(animal);
+        System.out.println("______________________");
     }
 }
